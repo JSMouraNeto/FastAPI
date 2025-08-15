c1d90ca577ca1fc19bdf5be164131de6c49682b2
CROMABLUE <jsmoura.dev@gmail.com>
2025-08-15 18:23:43 -0400

    :recycle: Refactor: remove the pids of the tables

--- backend/models.py
+++ backend/models.py
@@ -1,35 +1,28 @@
 # models.py
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
+from datetime import datetime
 
 class Metrica(SQLModel, table=True):
-
-    timestamp: str = Field(index=True, primary_key=True)
+    timestamp: datetime = Field(index=True, primary_key=True)
     usagetime: str
     delta_cpu_time: str
     cpu_usage: float
     rx_data: int
     tx_data: int
-   
-   
+    
     item_id: Optional[int] = Field(default=None, foreign_key="item.id")
-    # Propriedade de relacionamento para acessar o 'Item' pai a partir de uma 'Metrica'
     item: Optional["Item"] = Relationship(back_populates="metricas")
 
 class Item(SQLModel, table=True):
-   
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     package_name: str
     uid: int
-    pids: str  # Mantemos o pids como está
-   
     
     metricas: List["Metrica"] = Relationship(back_populates="item")
 
-
 class MetricaRead(SQLModel):
-   
-    timestamp: str
+    timestamp: datetime
     usagetime: str
     delta_cpu_time: str
     cpu_usage: float
@@ -37,11 +30,9 @@ class MetricaRead(SQLModel):
     tx_data: int
 
 class ItemReadWithMetrics(SQLModel):
-
     id: int
     package_name: str
     uid: int
-    pids: str
     metricas: List[MetricaRead] = []
 
 Metrica.model_rebuild()
